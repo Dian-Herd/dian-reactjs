78cd1aa4eb87d44e1b4c27b78d1cddf896228b3a
Dian-Herd <dian92943@gmail.com>
2022-02-16 02:12:04 +0700

    menambahkan string Oke sudah ditambah

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br />
+        Oke sudah ditambah
         <a
           className="App-link"
           href="https://reactjs.org"
