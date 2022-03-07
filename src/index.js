af1598e75eee6131836dcf7ad085a0c658077276
Dian-Herd <dian92943@gmail.com>
2022-03-07 16:31:55 +0700

    tugas Redux Dian Herdiana

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import Counter from './component/Counter';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+      <Counter />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
